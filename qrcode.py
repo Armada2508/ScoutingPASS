4417f41424a529abbb2e599ab1b5b84cd546c21d
Tricks1228 <christopp1228@gmail.com>
2024-02-10 15:02:40 -0600

    fixed qr reader, made fileimgread

--- qrcode.py
+++ qrcode.py
@@ -1,48 +1,70 @@
+import glob
 import time
 
 import cv2
-from pynput.keyboard import Controller, Key
+import pyautogui
+import pyperclip
+from pynput.keyboard import Controller, Key, Listener
 
-keyboard = Controller()
-qcd = cv2.QRCodeDetector()
-camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+#keyboard = Controller()
+qrCodeDetector = cv2.QRCodeDetector()
+camera = cv2.VideoCapture(0)
 
 while not camera.isOpened():
 	print("Attempting to access camera stream")
 	time.sleep(1)
 print("Camera stream opened")
 
-def pressAndRelease(key):
-	keyboard.press(key)
-	time.sleep(0.05)
-	keyboard.release(key)
- 
-def press(*keys):
-	for key in keys:
-		keyboard.press(key)
-		time.sleep(0.20)
- 
-def release(*keys):
-	for key in keys:
-		keyboard.release(key)
 
-def addToSheets(data):
-	keyboard.type(data)
-	pressAndRelease(Key.enter)
-	pressAndRelease(Key.up)
-	press(Key.alt_l, 'd', 'e')
-	release(Key.alt_l, 'd', 'e')
-	pressAndRelease(Key.esc)
-	pressAndRelease(Key.down)
+def addToSheets(text):
+    pyperclip.copy(text + "\n")
+    pyautogui.hotkey('ctrl', 'v')
+ 
+prev_codes = []
+escPressed = True
 
-prev_codes = ()
+def onEscPressed(e):
+    print("Test")
+    global escPressed
+    escPressed = True
+
 while True:
+	qrstr = ""
 	ret, frame = camera.read()
-	retval, qrcodes, points, straight_qrcode = qcd.detectAndDecodeMulti(frame)
-	if (qrcodes != ()):
-		if (qrcodes == prev_codes): continue
-		for qrcode in qrcodes:
-			if (qrcode != None and qrcode != ""):
-				print(qrcode)
-				addToSheets(qrcode)
-		prev_codes = qrcodes
+	qrcode, points, ret = qrCodeDetector.detectAndDecode(frame)
+	if qrcode: 
+		qrstr = str(qrcode)
+	cv2.imshow("Frame", frame)
+	if (qrcode in prev_codes or qrstr == ""): 
+		if cv2.waitKey(1) == ord('q'):
+			camera.release()
+			break	
+		continue
+	
+	if cv2.waitKey(1) == ord('q'):
+		camera.release()
+		break
+	addToSheets(qrcode)
+	prev_codes = qrcode
+	
+
+def fileImgRead(filePath):
+	def readThroughFolder(filePath):
+		imgFileList = glob.glob(filePath)
+		readImgList = []
+		for img in imgFileList:
+			print(img)
+			readImgList.append(cv2.imread(img))
+		return readImgList
+
+	imgList = readThroughFolder(filePath)
+	for img in imgList:
+		qrcode, stupidValue, a = qrCodeDetector.detectAndDecode(img)
+		#for qrcode in qrcode:
+		if qrcode:
+			qrstr = str(qrcode)
+		if (qrcode != None and qrcode != ""):
+			print(qrcode)
+			#addToSheets(qrcode)
+
+			
